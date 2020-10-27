c4f596dcc4df18848570d3c083158b820c5b05fd
Maxwell Weru <mburumaxwell@gmail.com>
2020-10-27 14:36:44 +0300

    Added support for health checks

---
+++ Tingle.EventBus.Health/EventBusHealthCheck.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Tingle.EventBus.Abstractions;
+
+namespace Tingle.EventBus.Health
+{
+    /// <summary>
+    /// Implementation of <see cref="IHealthCheck"/> for an <see cref="IEventBus"/>
+    /// </summary>
+    /// <typeparam name="TClient"></typeparam>
+    public class EventBusHealthCheck : IHealthCheck
+    {
+        private readonly IEventBus eventBus;
+
+        /// <summary>
+        /// Creates an instance of <see cref="EventBusHealthCheck"/>
+        /// </summary>
+        /// <param name="eventBus">The instance of <see cref="IEventBus"/> to use.</param>
+        public EventBusHealthCheck(IEventBus eventBus)
+        {
+            this.eventBus = eventBus ?? throw new ArgumentNullException(nameof(eventBus));
+        }
+
+        /// <inheritdoc/>
+        public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
+        {
+            try
+            {
+                _ = await eventBus.CheckHealthAsync(cancellationToken);
+                return HealthCheckResult.Healthy();
+            }
+            catch (Exception ex)
+            {
+                return new HealthCheckResult(context.Registration.FailureStatus, exception: ex);
+            }
+        }
+    }
+}
---
+++ Tingle.EventBus.Health/IHealthChecksBuilderExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Tingle.EventBus.Abstractions;
+using Tingle.EventBus.Health;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    /// <summary>
+    /// Extension methods on <see cref="IHealthChecksBuilder"/> for <see cref="IEventBus"/>.
+    /// </summary>
+    public static class IHealthChecksBuilderExtensions
+    {
+        /// <summary>
+        /// Add a health check for the event bus.
+        /// <see cref="IEventBus"/> must be resolvable from the services provider.
+        /// </summary>
+        /// <param name="builder">The <see cref="IHealthChecksBuilder"/>.</param>
+        /// <param name="name">The health check name.</param>
+        /// <returns>The <see cref="IHealthChecksBuilder"/>.</returns>
+        public static IHealthChecksBuilder AddEventBus(this IHealthChecksBuilder builder, string name = "eventbus")
+        {
+            return builder.AddCheck<EventBusHealthCheck>(name);
+        }
+    }
+}

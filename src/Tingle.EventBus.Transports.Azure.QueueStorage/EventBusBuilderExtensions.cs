ca419e38a239eee0288404a8a40803fb4a85c8ac
Maxwell Weru <mburumaxwell@gmail.com>
2020-12-17 19:54:27 +0300

    Added extension on EventBusBuilder for AzureQueueStorage

---
+++ src/Tingle.EventBus.Transports.Azure.QueueStorage/EventBusBuilderExtensions.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Tingle.EventBus;
+using Tingle.EventBus.Transports.Azure.QueueStorage;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    /// <summary>
+    /// Extension methods on <see cref="EventBusBuilder"/> for Azure Queue Storage.
+    /// </summary>
+    public static class EventBusBuilderExtensions
+    {
+        /// <summary>
+        /// Add Azure Queue Storage as the underlying transport for the Event Bus.
+        /// </summary>
+        /// <param name="builder"></param>
+        /// <param name="configure"></param>
+        /// <returns></returns>
+        public static EventBusBuilder AddAzureQueueStorage(this EventBusBuilder builder, Action<AzureQueueStorageOptions> configure)
+        {
+            if (builder == null) throw new ArgumentNullException(nameof(builder));
+            if (configure is null) throw new ArgumentNullException(nameof(configure));
+
+            var services = builder.Services;
+
+            // configure the options for Azure Service Bus
+            services.Configure(configure);
+            services.PostConfigure<AzureQueueStorageOptions>(options =>
+            {
+                // ensure the connection string
+                if (string.IsNullOrWhiteSpace(options.ConnectionString))
+                {
+                    throw new InvalidOperationException($"The '{nameof(options.ConnectionString)}' must be provided");
+                }
+            });
+
+            // register the event bus
+            services.AddSingleton<IEventBus, AzureQueueStorageEventBus>();
+
+            return builder;
+        }
+    }
+}

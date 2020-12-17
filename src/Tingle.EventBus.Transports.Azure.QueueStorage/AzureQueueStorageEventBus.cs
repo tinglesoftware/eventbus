5fc0ce0f799bd0308d433ca9259c184aa8519562
Maxwell Weru <mburumaxwell@gmail.com>
2020-12-17 19:28:22 +0300

    Publish support for Azure Queue Storage

--- src/Tingle.EventBus.Transports.Azure.QueueStorage/AzureQueueStorageEventBus.cs
+++ src/Tingle.EventBus.Transports.Azure.QueueStorage/AzureQueueStorageEventBus.cs
@@ -5,6 +5,8 @@ using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -15,8 +17,10 @@ namespace Tingle.EventBus.Transports.Azure.QueueStorage
     /// </summary>
     public class AzureQueueStorageEventBus : EventBusBase<AzureQueueStorageOptions>
     {
-        //private readonly QueueClient queueClient;
+        private const string SequenceNumberSeparator = "|";
         private readonly QueueServiceClient serviceClient;
+        private readonly Dictionary<Type, QueueClient> queueClientsCache = new Dictionary<Type, QueueClient>();
+        private readonly SemaphoreSlim queueClientsCacheLock = new SemaphoreSlim(1, 1); // only one at a time.
         private readonly ILogger logger;
 
         /// <summary>
@@ -46,15 +50,71 @@ namespace Tingle.EventBus.Transports.Azure.QueueStorage
         }
 
         /// <inheritdoc/>
-        public override Task<string> PublishAsync<TEvent>(EventContext<TEvent> @event, DateTimeOffset? scheduled = null, CancellationToken cancellationToken = default)
+        public override async Task<string> PublishAsync<TEvent>(EventContext<TEvent> @event,
+                                                                DateTimeOffset? scheduled = null,
+                                                                CancellationToken cancellationToken = default)
         {
-            throw new NotImplementedException();
+            var reg = BusOptions.GetRegistration<TEvent>();
+            using var ms = new MemoryStream();
+            var contentType = await SerializeAsync(body: ms,
+                                                   @event: @event,
+                                                   serializerType: reg.EventSerializerType,
+                                                   cancellationToken: cancellationToken);
+
+
+            // if scheduled for later, calculate the visibility timeout
+            var visibilityTimeout = scheduled - DateTimeOffset.UtcNow;
+
+            // if expiry is set, calculate the ttl
+            var ttl = @event.Expires - DateTimeOffset.UtcNow;
+
+            // get the queue client and send the message
+            var queueClient = await GetQueueClientAsync(reg, cancellationToken);
+            var message = Encoding.UTF8.GetString(ms.ToArray());
+            var response = await queueClient.SendMessageAsync(messageText: message,
+                                                              visibilityTimeout: visibilityTimeout,
+                                                              timeToLive: ttl,
+                                                              cancellationToken: cancellationToken);
+
+            // return the sequence number; both MessageId and PopReceipt are needed to update or delete
+            return scheduled != null ? string.Join(SequenceNumberSeparator, response.Value.MessageId, response.Value.PopReceipt) : null;
         }
 
         /// <inheritdoc/>
-        public override Task<IList<string>> PublishAsync<TEvent>(IList<EventContext<TEvent>> events, DateTimeOffset? scheduled = null, CancellationToken cancellationToken = default)
+        public override async Task<IList<string>> PublishAsync<TEvent>(IList<EventContext<TEvent>> events, DateTimeOffset? scheduled = null, CancellationToken cancellationToken = default)
         {
-            throw new NotImplementedException();
+            // log warning when doing batch
+            logger.LogWarning("Azure Queue Storage does not support batching. The events will be looped through one by one");
+
+            // work on each event
+            var reg = BusOptions.GetRegistration<TEvent>();
+            var sequenceNumbers = new List<string>();
+            var queueClient = await GetQueueClientAsync(reg, cancellationToken);
+            foreach (var @event in events)
+            {
+                using var ms = new MemoryStream();
+                var contentType = await SerializeAsync(body: ms,
+                                                       @event: @event,
+                                                       serializerType: reg.EventSerializerType,
+                                                       cancellationToken: cancellationToken);
+                // if scheduled for later, calculate the visibility timeout
+                var visibilityTimeout = scheduled - DateTimeOffset.UtcNow;
+
+                // if expiry is set, calculate the ttl
+                var ttl = @event.Expires - DateTimeOffset.UtcNow;
+
+                // send the message
+                var message = Encoding.UTF8.GetString(ms.ToArray());
+                var response = await queueClient.SendMessageAsync(messageText: message,
+                                                                  visibilityTimeout: visibilityTimeout,
+                                                                  timeToLive: ttl,
+                                                                  cancellationToken: cancellationToken);
+                // collect the sequence number
+                sequenceNumbers.Add(string.Join(SequenceNumberSeparator, response.Value.MessageId, response.Value.PopReceipt));
+            }
+
+            // return the sequence number
+            return scheduled != null ? sequenceNumbers : null;
         }
 
         /// <inheritdoc/>
@@ -67,6 +127,33 @@ namespace Tingle.EventBus.Transports.Azure.QueueStorage
         public override Task StopAsync(CancellationToken cancellationToken)
         {
             throw new NotImplementedException();
+        }
+
+        private async Task<QueueClient> GetQueueClientAsync(EventConsumerRegistration reg, CancellationToken cancellationToken)
+        {
+            await queueClientsCacheLock.WaitAsync(cancellationToken);
+
+            try
+            {
+                if (!queueClientsCache.TryGetValue(reg.EventType, out var queueClient))
+                {
+                    var name = reg.EventName;
+
+                    // create the queue client
+                    queueClient = new QueueClient(connectionString: TransportOptions.ConnectionString, queueName: name);
+
+                    // ensure queue is created
+                    await queueClient.CreateIfNotExistsAsync(cancellationToken: cancellationToken);
+
+                    queueClientsCache[reg.EventType] = queueClient;
+                };
+
+                return queueClient;
+            }
+            finally
+            {
+                queueClientsCacheLock.Release();
+            }
         }
     }
 }

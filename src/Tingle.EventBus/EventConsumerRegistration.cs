11e5bc7036c75dc6fd84820e5fca7656342a8566
Maxwell Weru <mburumaxwell@gmail.com>
2020-12-20 09:40:37 +0300

    Added EventRegistration as the base class for EventConsumerRegistration

--- src/Tingle.EventBus/EventConsumerRegistration.cs
+++ src/Tingle.EventBus/EventConsumerRegistration.cs
@@ -5,23 +5,17 @@ namespace Tingle.EventBus
     /// <summary>
     /// Represents a registration for a consumer of an event.
     /// </summary>
-    public class EventConsumerRegistration
+    public class EventConsumerRegistration : EventRegistration
     {
         /// <summary>
         /// Creates an instance of <see cref="EventConsumerRegistration"/>
         /// </summary>
         /// <param name="eventType">The type of event handled.</param>
         /// <param name="consumerType">The type of consumer handling the event.</param>
-        public EventConsumerRegistration(Type eventType, Type consumerType)
+        public EventConsumerRegistration(Type eventType, Type consumerType) : base(eventType)
         {
-            EventType = eventType ?? throw new ArgumentNullException(nameof(eventType));
             ConsumerType = consumerType ?? throw new ArgumentNullException(nameof(consumerType));
         }
-
-        /// <summary>
-        /// The type of event handled.
-        /// </summary>
-        public Type EventType { get; }
 
         /// <summary>
         /// The type of consumer handling the event.
@@ -29,19 +23,8 @@ namespace Tingle.EventBus
         public Type ConsumerType { get; }
 
         /// <summary>
-        /// The name generated for the event.
-        /// </summary>
-        public string EventName { get; set; }
-
-        /// <summary>
         /// The name generated for the consumer.
         /// </summary>
         public string ConsumerName { get; set; }
-
-        /// <summary>
-        /// The type used for serializing and deserializing events.
-        /// This type must implement <see cref="Serialization.IEventSerializer"/>.
-        /// </summary>
-        public Type EventSerializerType { get; set; }
     }
 }
---
+++ src/Tingle.EventBus/EventRegistration.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Tingle.EventBus
+{
+    /// <summary>
+    /// Represents a registration for an event.
+    /// </summary>
+    public class EventRegistration
+    {
+        /// <summary>
+        /// Creates an instance of <see cref="EventRegistration"/>
+        /// </summary>
+        /// <param name="eventType">The type of event handled.</param>
+        public EventRegistration(Type eventType)
+        {
+            EventType = eventType ?? throw new ArgumentNullException(nameof(eventType));
+        }
+
+        /// <summary>
+        /// The type of event handled.
+        /// </summary>
+        public Type EventType { get; }
+
+        /// <summary>
+        /// The name generated for the event.
+        /// </summary>
+        public string EventName { get; set; }
+
+        /// <summary>
+        /// The type used for serializing and deserializing events.
+        /// This type must implement <see cref="Serialization.IEventSerializer"/>.
+        /// </summary>
+        public Type EventSerializerType { get; set; }
+    }
+}

fc47ddd5126cdc1fa10b4fa34aaf0abddf46da47
Maxwell Weru <mburumaxwell@gmail.com>
2020-12-20 10:17:57 +0300

    Move InternalsVisibleTo.cs into the project file

--- src/Tingle.EventBus/InternalsVisibleTo.cs
+++
@@ -1,5 +0,0 @@
-﻿using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("Tingle.EventBus.Transports.AzureServiceBus")]
-[assembly: InternalsVisibleTo("Tingle.EventBus.Transports.RabbitMQ")]
-[assembly: InternalsVisibleTo("Tingle.EventBus.Transports.InMemory")]

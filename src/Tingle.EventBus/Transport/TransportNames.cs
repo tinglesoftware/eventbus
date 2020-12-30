f46ff43bb0ffd82f6f1d6e05b6cb97c487846ca1
Maxwell Weru <mburumaxwell@gmail.com>
2020-12-30 09:08:36 +0300

    Added names of common transports

---
+++ src/Tingle.EventBus/Transport/TransportNames.cs
@@ -0,0 +1,19 @@
+﻿namespace Tingle.EventBus.Transport
+{
+    /// <summary>
+    /// The names of known transports
+    /// </summary>
+    public static class TransportNames
+    {
+#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+        public const string AmazonKinesis = "amazon-kinesis";
+        public const string AmazonSqs = "amazon-sqs";
+        public const string AzureEventHubs = "azure-event-hubs";
+        public const string AzureQueueStorage = "azure-queue-storage";
+        public const string AzureServiceBus = "azure-service-bus";
+        public const string InMemory = "inmemory";
+        public const string Kafka = "kafka";
+        public const string RabbitMq = "rabbitmq";
+#pragma warning restore CS1591 // Missing XML comment for publicly visible type or member
+    }
+}

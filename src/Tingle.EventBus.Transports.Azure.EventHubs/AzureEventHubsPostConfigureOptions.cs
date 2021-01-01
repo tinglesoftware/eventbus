c67363ddb7cb6d6bd5ed734a3a84016178b476ec
Maxwell Weru <mburumaxwell@gmail.com>
2021-01-01 12:20:21 +0300

    Azure Blob Storage configuration settings is only required if there are consumers for Azure Event Bus

---
+++ src/Tingle.EventBus.Transports.Azure.EventHubs/AzureEventHubsPostConfigureOptions.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using Tingle.EventBus;
+
+namespace Microsoft.Extensions.Options
+{
+    /// <summary>
+    /// A class to finish the configuration of instances of <see cref="AzureEventHubsOptions"/>.
+    /// </summary>
+    internal class AzureEventHubsPostConfigureOptions : IPostConfigureOptions<AzureEventHubsOptions>
+    {
+        private readonly EventBusOptions busOptions;
+
+        public AzureEventHubsPostConfigureOptions(IOptions<EventBusOptions> busOptionsAccessor)
+        {
+            busOptions = busOptionsAccessor?.Value ?? throw new ArgumentNullException(nameof(busOptionsAccessor));
+        }
+
+        public void PostConfigure(string name, AzureEventHubsOptions options)
+        {
+            // ensure the connection string
+            if (string.IsNullOrWhiteSpace(options.ConnectionString))
+            {
+                throw new InvalidOperationException($"The '{nameof(options.ConnectionString)}' must be provided");
+            }
+
+            // if there are consumers for this transport, we must check azure blob storage
+            var consumers = busOptions.GetConsumerRegistrations(TransportNames.AzureEventHubs);
+            if (consumers.Count > 0)
+            {
+                // ensure the connection string for blob storage is valid
+                if (string.IsNullOrWhiteSpace(options.BlobStorageConnectionString))
+                {
+                    throw new InvalidOperationException($"The '{nameof(options.BlobStorageConnectionString)}' must be provided");
+                }
+
+                // ensure the blob container name is provided
+                if (string.IsNullOrWhiteSpace(options.BlobContainerName))
+                {
+                    throw new InvalidOperationException($"The '{nameof(options.BlobContainerName)}' must be provided");
+                }
+
+                // ensure the prefix is always lower case.
+                options.BlobContainerName = options.BlobContainerName.ToLower();
+            }
+        }
+    }
+}
--- src/Tingle.EventBus.Transports.Azure.EventHubs/EventBusBuilderExtensions.cs
+++ src/Tingle.EventBus.Transports.Azure.EventHubs/EventBusBuilderExtensions.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Options;
+using System;
 using Tingle.EventBus.Transports.Azure.EventHubs;
 
 namespace Microsoft.Extensions.DependencyInjection
@@ -23,29 +25,8 @@ namespace Microsoft.Extensions.DependencyInjection
 
             // configure the options for Azure Service Bus
             services.Configure(configure);
-            services.PostConfigure<AzureEventHubsOptions>(options =>
-            {
-                // ensure the connection string
-                if (string.IsNullOrWhiteSpace(options.ConnectionString))
-                {
-                    throw new InvalidOperationException($"The '{nameof(options.ConnectionString)}' must be provided");
-                }
-
-                // ensure the connection string for blob storage is valid
-                if (string.IsNullOrWhiteSpace(options.BlobStorageConnectionString))
-                {
-                    throw new InvalidOperationException($"The '{nameof(options.BlobStorageConnectionString)}' must be provided");
-                }
-
-                // ensure the blob container name is provided
-                if (string.IsNullOrWhiteSpace(options.BlobContainerName))
-                {
-                    throw new InvalidOperationException($"The '{nameof(options.BlobContainerName)}' must be provided");
-                }
-
-                // ensure the prefix is always lower case.
-                options.BlobContainerName = options.BlobContainerName.ToLower();
-            });
+            services.TryAddEnumerable(
+                ServiceDescriptor.Singleton<IPostConfigureOptions<AzureEventHubsOptions>, AzureEventHubsPostConfigureOptions>());
 
             // register the transport
             builder.RegisterTransport<AzureEventHubsTransport>();

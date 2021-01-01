cabeb6c57ab461234257f62f2327a1a537cdd8ef
Maxwell Weru <mburumaxwell@gmail.com>
2021-01-01 11:18:04 +0300

    Added examples to properties in HostInfo

--- src/Tingle.EventBus/Serialization/HostInfo.cs
+++ src/Tingle.EventBus/Serialization/HostInfo.cs
@@ -8,31 +8,37 @@
         /// <summary>
         /// The machine name (or role instance name) of the machine.
         /// </summary>
+        /// <example>WIN-HQ1243</example>
         public string MachineName { get; internal set; }
 
         /// <summary>
         /// The name of the application.
         /// </summary>
+        /// <example>Tingle.EventBus.Examples.SimplePublisher</example>
         public string ApplicationName { get; internal set; }
 
         /// <summary>
         /// The version of the application.
         /// </summary>
+        /// <example>1.0.0.0</example>
         public string ApplicationVersion { get; internal set; }
 
         /// <summary>
         /// The name of the environment the application is running in.
         /// </summary>
+        /// <example>Production</example>
         public string EnvironmentName { get; internal set; }
 
         /// <summary>
         /// The version of the library.
         /// </summary>
+        /// <example>1.0.0.0</example>
         public string LibraryVersion { get; internal set; }
 
         /// <summary>
         /// The operating system hosting the application.
         /// </summary>
+        /// <example>Microsoft Windows NT 10.0.19042.0</example>
         public string OperatingSystem { get; internal set; }
     }
 }

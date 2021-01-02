2c9733ff5ba337a8a4b44481a125537230759dca
Maxwell Weru <mburumaxwell@gmail.com>
2021-01-02 08:44:05 +0300

    Prepare ActivitySource

---
+++ src/Tingle.EventBus/Diagnostics/EventBusActivitySource.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+
+namespace Tingle.EventBus.Diagnostics
+{
+    internal class EventBusActivitySource
+    {
+        internal static readonly AssemblyName AssemblyName = typeof(EventBusActivitySource).Assembly.GetName();
+        internal static readonly string ActivitySourceName = AssemblyName.Name;
+        internal static readonly Version Version = AssemblyName.Version;
+        internal static readonly ActivitySource ActivitySource = new ActivitySource(ActivitySourceName, Version.ToString());
+    }
+}

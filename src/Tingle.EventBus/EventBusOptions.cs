1a2efd5af10947418a7f726509743119f213f827
Maxwell Weru <mburumaxwell@gmail.com>
2021-01-29 09:38:28 +0300

    Merge EventBusOptionsExtensions into EventBusOptions

--- src/Tingle.EventBus/EventBusOptions.cs
+++ src/Tingle.EventBus/EventBusOptions.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text.Json;
 using Tingle.EventBus.Registrations;
 using Tingle.EventBus.Serialization;
@@ -83,22 +84,6 @@ namespace Tingle.EventBus
         internal HostInfo HostInfo { get; set; }
 
         /// <summary>
-        /// The registrations for events and consumers for the EventBus.
-        /// </summary>
-        internal Dictionary<Type, EventRegistration> Registrations { get; } = new Dictionary<Type, EventRegistration>();
-
-        /// <summary>
-        /// Gets or sets the name of the default transport.
-        /// When there is only one transport registered, setting this value is not necessary, as it is used as the default.
-        /// </summary>
-        public string DefaultTransportName { get; set; }
-
-        /// <summary>
-        /// The map of registered transport names to their types.
-        /// </summary>
-        internal Dictionary<string, Type> RegisteredTransportNames { get; } = new Dictionary<string, Type>();
-
-        /// <summary>
         /// Indicates if the messages/events procuded require guard against duplicate messages.
         /// If <see langword="true"/>, duplicate messages having the same <see cref="EventContext.Id"/>
         /// sent to the same destination within a duration of <see cref="DuplicateDetectionDuration"/> will be discarded.
@@ -119,6 +104,79 @@ namespace Tingle.EventBus
         /// </remarks>
         public TimeSpan DuplicateDetectionDuration { get; set; } = TimeSpan.FromMinutes(1);
 
+        /// <summary>
+        /// Gets or sets the name of the default transport.
+        /// When there is only one transport registered, setting this value is not necessary, as it is used as the default.
+        /// </summary>
+        public string DefaultTransportName { get; set; }
+
+        /// <summary>
+        /// The map of registered transport names to their types.
+        /// </summary>
+        internal Dictionary<string, Type> RegisteredTransportNames { get; } = new Dictionary<string, Type>();
+
+        /// <summary>
+        /// The registrations for events and consumers for the EventBus.
+        /// </summary>
+        internal Dictionary<Type, EventRegistration> Registrations { get; } = new Dictionary<Type, EventRegistration>();
+
+        /// <summary>
+        /// Gets the consumer registrations for a given transport.
+        /// </summary>
+        /// <param name="transportName">The name of the transport for whom to get registered consumers.</param>
+        /// <returns></returns>
+        public ICollection<EventRegistration> GetRegistrations(string transportName)
+        {
+            if (string.IsNullOrWhiteSpace(transportName))
+            {
+                throw new ArgumentException($"'{nameof(transportName)}' cannot be null or whitespace", nameof(transportName));
+            }
+
+            // filter out the consumers where the event is set for the given transport
+            return Registrations.Values.Where(r => r.TransportName == transportName).ToList();
+        }
+
+        /// <summary>
+        /// Get or create the event registration for a given event type.
+        /// </summary>
+        /// <typeparam name="TEvent">The event type to retrieve an <see cref="EventRegistration"/> for.</typeparam>
+        /// <returns></returns>
+        /// <exception cref="KeyNotFoundException">The given event type does not have any event registered.</exception>
+        public EventRegistration GetOrCreateRegistration<TEvent>()
+        {
+            // if there's already a registration for the event return it
+            var eventType = typeof(TEvent);
+            if (Registrations.TryGetValue(key: eventType, out var registration)) return registration;
+
+            // at this point, the registration does not exist; create it and add to the registrations for repeated use
+            registration = new EventRegistration(eventType);
+            registration.SetSerializer() // set serializer
+                        .SetEventName(this) // set event name
+                        .SetTransportName(this); // set transport name
+            return Registrations[eventType] = registration;
+        }
+
+        /// <summary>
+        /// Get the consumer registration in a given event type.
+        /// </summary>
+        /// <typeparam name="TEvent">The event type from wich to retrieve a <see cref="EventConsumerRegistration"/> for.</typeparam>
+        /// <typeparam name="TConsumer">The consumer to configure.</typeparam>
+        /// <param name="registration">
+        /// When this method returns, contains the consumer registration associated with the specified event type,
+        /// if the event type is found; otherwise, <see langword="null"/> is returned.
+        /// This parameter is passed uninitialized.
+        /// </param>
+        /// <returns><see langword="true" /> if there's a consumer registered for the given event type; otherwise, false.</returns>
+        public bool TryGetConsumerRegistration<TEvent, TConsumer>(out EventConsumerRegistration registration)
+        {
+            registration = default;
+            if (Registrations.TryGetValue(typeof(TEvent), out var ereg))
+            {
+                registration = ereg.Consumers.SingleOrDefault(cr => cr.ConsumerType == typeof(TConsumer));
+                if (registration != null) return true;
+            }
+            return false;
+        }
 
         /// <summary>
         /// Configure the <see cref="EventRegistration"/> for <typeparamref name="TEvent"/>.
@@ -130,7 +188,7 @@ namespace Tingle.EventBus
         {
             if (configure is null) throw new ArgumentNullException(nameof(configure));
 
-            var registration = this.GetOrCreateRegistration<TEvent>();
+            var registration = GetOrCreateRegistration<TEvent>();
             configure(registration);
 
             return this;
@@ -148,7 +206,7 @@ namespace Tingle.EventBus
         {
             if (configure is null) throw new ArgumentNullException(nameof(configure));
 
-            if (this.TryGetConsumerRegistration<TEvent, TConsumer>(out var registration))
+            if (TryGetConsumerRegistration<TEvent, TConsumer>(out var registration))
             {
                 configure(registration);
             }

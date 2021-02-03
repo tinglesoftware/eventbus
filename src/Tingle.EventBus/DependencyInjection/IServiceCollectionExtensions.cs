472d8979899798f0394a1ca4743ea3aaccdad2a4
Maxwell Weru <mburumaxwell@gmail.com>
2021-02-03 08:25:35 +0300

    Added overload of AddEventBus(...) extension that returns EventBusBuilder

--- src/Tingle.EventBus/DependencyInjection/IServiceCollectionExtensions.cs
+++ src/Tingle.EventBus/DependencyInjection/IServiceCollectionExtensions.cs
@@ -11,14 +11,26 @@ namespace Microsoft.Extensions.DependencyInjection
         /// Add Event Bus services.
         /// </summary>
         /// <param name="services">The <see cref="IServiceCollection"/> instance to add services to.</param>
+        /// <returns>An <see cref="EventBusBuilder"/> to continue setting up the Event Bus.</returns>
+        public static EventBusBuilder AddEventBus(this IServiceCollection services)
+        {
+            if (services == null) throw new ArgumentNullException(nameof(services));
+
+            return new EventBusBuilder(services)
+                .UseDefaultSerializer();
+        }
+
+        /// <summary>
+        /// Add Event Bus services.
+        /// </summary>
+        /// <param name="services">The <see cref="IServiceCollection"/> instance to add services to.</param>
         /// <param name="setupAction">An optional action for setting up the bus.</param>
         /// <returns></returns>
         public static IServiceCollection AddEventBus(this IServiceCollection services, Action<EventBusBuilder> setupAction = null)
         {
             if (services == null) throw new ArgumentNullException(nameof(services));
 
-            var builder = new EventBusBuilder(services)
-                .UseDefaultSerializer();
+            var builder = services.AddEventBus();
 
             setupAction?.Invoke(builder);
 

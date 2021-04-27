7c75bd1e2489db74ad85940a9a02b07b0f968c2b
Maxwell Weru <mburumaxwell@gmail.com>
2021-04-27 09:59:51 +0300

    Generate event Ids matching the format specified

---
+++ tests/Tingle.EventBus.Tests/EventBusTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Tingle.EventBus.Registrations;
+using Xunit;
+
+namespace Tingle.EventBus.Tests
+{
+    public class EventBusTests
+    {
+        [Theory]
+        [InlineData(EventIdFormat.Guid)]
+        [InlineData(EventIdFormat.GuidNoDashes)]
+        [InlineData(EventIdFormat.Long)]
+        [InlineData(EventIdFormat.LongHex)]
+        [InlineData(EventIdFormat.DoubleLong)]
+        [InlineData(EventIdFormat.DoubleLongHex)]
+        [InlineData(EventIdFormat.Random)]
+        public void GenerateEventId_Works(EventIdFormat format)
+        {
+            var reg = new EventRegistration(typeof(TestEvent1)) { IdFormat = format, };
+            var id = EventBus.GenerateEventId(reg);
+            Assert.NotNull(id);
+        }
+
+        [Fact]
+        public void GenerateEventId_Throws_InvalidOperationExecption()
+        {
+            var reg = new EventRegistration(typeof(TestEvent1)) { IdFormat = null, };
+            var ex = Assert.Throws<NotSupportedException>(() => EventBus.GenerateEventId(reg));
+            Assert.Equal($"'{nameof(EventIdFormat)}.{reg.IdFormat}' set on event '{reg.EventType.FullName}' is not supported.", ex.Message);
+        }
+    }
+}

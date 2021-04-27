1569253f5bfeac8175b3bf30212712b08638b42f
Maxwell Weru <mburumaxwell@gmail.com>
2021-04-27 09:06:09 +0300

    Added InMemoryTransportPostConfigureOptions

---
+++ src/Tingle.EventBus/Transports/InMemory/InMemoryTransportPostConfigureOptions.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Options;
+using System;
+using Tingle.EventBus;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    /// <summary>
+    /// A class to finish the configuration of instances of <see cref="InMemoryTransportOptions"/>.
+    /// </summary>
+    internal class InMemoryTransportPostConfigureOptions : IPostConfigureOptions<InMemoryTransportOptions>
+    {
+        private readonly EventBusOptions busOptions;
+
+        public InMemoryTransportPostConfigureOptions(IOptions<EventBusOptions> busOptionsAccessor)
+        {
+            busOptions = busOptionsAccessor?.Value ?? throw new ArgumentNullException(nameof(busOptionsAccessor));
+        }
+
+        public void PostConfigure(string name, InMemoryTransportOptions options)
+        {
+            var registrations = busOptions.GetRegistrations(TransportNames.InMemory);
+            foreach (var ereg in registrations)
+            {
+                // Do anything necessary here
+            }
+        }
+    }
+}

781f3cca1df83ca9a86559729cebbc024393c69f
Maxwell Weru <mburumaxwell@gmail.com>
2021-08-07 10:54:15 +0300

    Added IEventIdGenerator and its default implementation

---
+++ src/Tingle.EventBus/Ids/DefaultEventIdGenerator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Tingle.EventBus.Registrations;
+
+namespace Tingle.EventBus.Ids
+{
+    /// <summary>
+    /// Default implementation of <see cref="IEventIdGenerator"/>.
+    /// </summary>
+    public class DefaultEventIdGenerator : IEventIdGenerator
+    {
+        /// <inheritdoc/>
+        public string Generate(EventRegistration reg)
+        {
+            if (reg is null) throw new ArgumentNullException(nameof(reg));
+
+            var id = Guid.NewGuid();
+            var bytes = id.ToByteArray();
+
+            return reg.IdFormat switch
+            {
+                EventIdFormat.Guid => id.ToString(),
+                EventIdFormat.GuidNoDashes => id.ToString("n"),
+                EventIdFormat.Long => BitConverter.ToUInt64(bytes, 0).ToString(),
+                EventIdFormat.LongHex => BitConverter.ToUInt64(bytes, 0).ToString("x"),
+                EventIdFormat.DoubleLong => $"{BitConverter.ToUInt64(bytes, 0)}{BitConverter.ToUInt64(bytes, 8)}",
+                EventIdFormat.DoubleLongHex => $"{BitConverter.ToUInt64(bytes, 0):x}{BitConverter.ToUInt64(bytes, 8):x}",
+                EventIdFormat.Random => Convert.ToBase64String(bytes),
+                _ => throw new NotSupportedException($"'{nameof(EventIdFormat)}.{reg.IdFormat}' set on event '{reg.EventType.FullName}' is not supported."),
+            };
+        }
+    }
+}

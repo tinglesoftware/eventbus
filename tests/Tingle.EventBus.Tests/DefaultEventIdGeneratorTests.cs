b983530b8c29c22b9f9e191af96e5d6a219f2d61
Maxwell Weru <mburumaxwell@gmail.com>
2021-08-07 10:57:09 +0300

    Unit test DefaultEventIdGenerator

--- tests/Tingle.EventBus.Tests/DefaultEventIdGeneratorTests.cs
+++ tests/Tingle.EventBus.Tests/DefaultEventIdGeneratorTests.cs
@@ -1,10 +1,11 @@
 ﻿using System;
+using Tingle.EventBus.Ids;
 using Tingle.EventBus.Registrations;
 using Xunit;
 
 namespace Tingle.EventBus.Tests
 {
-    public class EventBusTests
+    public class DefaultEventIdGeneratorTests
     {
         [Theory]
         [InlineData(EventIdFormat.Guid)]
@@ -16,16 +17,18 @@ namespace Tingle.EventBus.Tests
         [InlineData(EventIdFormat.Random)]
         public void GenerateEventId_Works(EventIdFormat format)
         {
+            var generator = new DefaultEventIdGenerator();
             var reg = new EventRegistration(typeof(TestEvent1)) { IdFormat = format, };
-            var id = EventBus.GenerateEventId(reg);
+            var id = generator.Generate(reg);
             Assert.NotNull(id);
         }
 
         [Fact]
         public void GenerateEventId_Throws_InvalidOperationExecption()
         {
+            var generator = new DefaultEventIdGenerator();
             var reg = new EventRegistration(typeof(TestEvent1)) { IdFormat = null, };
-            var ex = Assert.Throws<NotSupportedException>(() => EventBus.GenerateEventId(reg));
+            var ex = Assert.Throws<NotSupportedException>(() => generator.Generate(reg));
             Assert.Equal($"'{nameof(EventIdFormat)}.{reg.IdFormat}' set on event '{reg.EventType.FullName}' is not supported.", ex.Message);
         }
     }

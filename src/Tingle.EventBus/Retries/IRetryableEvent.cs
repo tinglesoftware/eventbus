6ba61ae8f9b8e024c8e4b6a707e10f695ab6fd9c
Maxwell Weru <mburumaxwell@gmail.com>
2021-09-12 18:52:17 +0300

    Retryable event (#286)
    * Added IRetryableEvent and AbstractRetryableEvent
    
    * Added extensions for IRetryableEvent with Polly support for generating delays
    
    * Rename command to @event
    
    * Added methods to schedule a retry

---
+++ src/Tingle.EventBus/Retries/IRetryableEvent.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Tingle.EventBus.Retries
+{
+    /// <summary>
+    /// Represents an event (or command) that can be retried and carries its own retry schedule.
+    /// </summary>
+    public interface IRetryableEvent
+    {
+        /// <summary>List of delays to be used for this command.</summary>
+        IList<TimeSpan> Delays { get; set; }
+
+        /// <summary>List of previous attempts.</summary>
+        IList<DateTimeOffset> Attempts { get; set; }
+    }
+}

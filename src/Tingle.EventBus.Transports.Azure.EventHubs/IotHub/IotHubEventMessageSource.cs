db03afbb712e888854fa56c5f45c7ac07f5f4c48
Maxwell Weru <mburumaxwell@gmail.com>
2021-12-27 17:57:17 +0300

    Added some enums for Azure IoT Hub

---
+++ src/Tingle.EventBus.Transports.Azure.EventHubs/IotHub/IotHubEventMessageSource.cs
@@ -0,0 +1,14 @@
+﻿namespace Tingle.EventBus.Transports.Azure.EventHubs.IotHub;
+
+/// <summary>The kind of source for an Azure IoT Hub Event.</summary>
+public enum IotHubEventMessageSource
+{
+    ///
+    Telemetry,
+
+    ///
+    TwinChangeEvents,
+
+    ///
+    DeviceLifecycleEvents,
+}

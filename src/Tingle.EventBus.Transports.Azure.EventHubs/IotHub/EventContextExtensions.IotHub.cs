400b4ae63d1b72e0749198ca9477bf954778c6d5
Maxwell Weru <mburumaxwell@gmail.com>
2021-12-27 14:42:12 +0300

    Added extensions to get iothub-connection-auth-method, iothub-connection-auth-generation-id and iothub-enqueuedtime

--- src/Tingle.EventBus.Transports.Azure.EventHubs/IotHub/EventContextExtensions.IotHub.cs
+++ src/Tingle.EventBus.Transports.Azure.EventHubs/IotHub/EventContextExtensions.IotHub.cs
@@ -1,4 +1,5 @@
 ﻿using System.Diagnostics.CodeAnalysis;
+using Tingle.EventBus.Transports.Azure.EventHubs.IotHub;
 
 namespace Tingle.EventBus;
 
@@ -8,8 +9,11 @@ namespace Tingle.EventBus;
 public static partial class EventContextExtensions
 {
     private const string IotHubPropertyNameMessageId = "message-id";
+    private const string IotHubPropertyNameEnqueuedTime = "iothub-enqueuedtime";
     private const string IotHubPropertyNameDeviceId = "iothub-connection-device-id";
     private const string IotHubPropertyNameModuleId = "iothub-connection-module-id";
+    private const string IotHubPropertyNameConnectionAuthGenerationId = "iothub-connection-auth-generation-id";
+    private const string IotHubPropertyNameConnectionAuthMethod = "iothub-connection-auth-method";
     private const string IotHubPropertyNameMessageSource = "iothub-message-source";
     private const string IotHubPropertyNameDataSchema = "dt-dataschema";
     private const string IotHubPropertyNameSubject = "dt-subject";
@@ -20,8 +24,13 @@ public static partial class EventContextExtensions
 
     private static bool TryGetIotHubPropertyValue(this EventContext context, string key, [NotNullWhen(true)] out object? value)
     {
-        value = null;
         if (context is null) throw new ArgumentNullException(nameof(context));
+        if (string.IsNullOrWhiteSpace(key))
+        {
+            throw new ArgumentException($"'{nameof(key)}' cannot be null or whitespace.", nameof(key));
+        }
+
+        value = null;
 
         if (context.TryGetEventData(out var eventData))
         {
@@ -30,6 +39,11 @@ public static partial class EventContextExtensions
         }
 
         return false;
+    }
+
+    private static T? GetIotHubPropertyValue<T>(this EventContext context, string key)
+    {
+        return context.TryGetIotHubPropertyValue(key, out var value) && value is not null ? (T?)value : default;
     }
 
     private static string? GetIotHubStringPropertyValue(this EventContext context, string key)
@@ -46,6 +60,10 @@ public static partial class EventContextExtensions
     /// <param name="context">The <see cref="EventContext"/> to use.</param>
     public static string? GetIotHubMessageId(this EventContext context) => context.GetIotHubStringPropertyValue(IotHubPropertyNameMessageId);
 
+    /// <summary>Gets the enqueued time for the IoT Hub message.</summary>
+    /// <param name="context">The <see cref="EventContext"/> to use.</param>
+    public static DateTime? GetIotHubEnqueuedTime(this EventContext context) => context.GetIotHubPropertyValue<DateTime>(IotHubPropertyNameEnqueuedTime);
+
     /// <summary>Gets the device identifier for the IoT Hub message.</summary>
     /// <param name="context">The <see cref="EventContext"/> to use.</param>
     public static string? GetIotHubDeviceId(this EventContext context) => context.GetIotHubStringPropertyValue(IotHubPropertyNameDeviceId);
@@ -57,6 +75,23 @@ public static partial class EventContextExtensions
     /// <summary>Gets the module identifier for the IoT Hub message.</summary>
     /// <param name="context">The <see cref="EventContext"/> to use.</param>
     public static string? GetIotHubModuleId(this EventContext context) => context.GetIotHubStringPropertyValue(IotHubPropertyNameModuleId);
+
+    /// <summary>Gets the connection authentication generation identifier for the IoT Hub message.</summary>
+    /// <param name="context">The <see cref="EventContext"/> to use.</param>
+    public static string? GetIotHubConnectionAuthGenerationId(this EventContext context) => context.GetIotHubStringPropertyValue(IotHubPropertyNameConnectionAuthGenerationId);
+
+    /// <summary>Gets the raw connection authentication method for the IoT Hub message.</summary>
+    /// <param name="context">The <see cref="EventContext"/> to use.</param>
+    public static string? GetIotHubConnectionAuthMethodRaw(this EventContext context) => context.GetIotHubStringPropertyValue(IotHubPropertyNameConnectionAuthMethod);
+
+    /// <summary>Gets the connection authentication method for the IoT Hub message.</summary>
+    /// <param name="context">The <see cref="EventContext"/> to use.</param>
+    public static IotHubConnectionAuthMethod? GetIotHubConnectionAuthMethod(this EventContext context)
+    {
+        var json = context.GetIotHubConnectionAuthMethodRaw();
+        if (string.IsNullOrEmpty(json)) return null;
+        return System.Text.Json.JsonSerializer.Deserialize<IotHubConnectionAuthMethod>(json);
+    }
 
     /// <summary>Gets the source for the IoT Hub message.</summary>
     /// <param name="context">The <see cref="EventContext"/> to use.</param>

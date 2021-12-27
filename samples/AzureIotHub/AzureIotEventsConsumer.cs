50a67b3be3a462c370faa487c8896ad57aec57f0
Maxwell Weru <mburumaxwell@gmail.com>
2021-12-27 12:50:45 +0300

    Added basic sample for Azure IoT Hub

---
+++ samples/AzureIotHub/AzureIotEventsConsumer.cs
@@ -0,0 +1,16 @@
+﻿using Azure.Messaging.EventHubs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureIotHub;
+
+internal class AzureIotEventsConsumer : IEventConsumer<EventData>
+{
+    public Task ConsumeAsync(EventContext<EventData> context, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ samples/AzureIotHub/Program.cs
@@ -0,0 +1,22 @@
+﻿using AzureIotHub;
+
+var host = Host.CreateDefaultBuilder(args)
+               .ConfigureServices((hostContext, services) =>
+               {
+                   var configuration = hostContext.Configuration;
+
+                   services.AddEventBus(builder =>
+                   {
+                       builder.AddConsumer<AzureIotEventsConsumer>();
+
+                       // Transport specific configuration
+                       builder.AddAzureEventHubsTransport(options =>
+                       {
+                           options.Credentials = configuration.GetConnectionString("EventHub");
+                           options.BlobStorageCredentials = "UseDevelopmentStorage=true;";
+                       });
+                   });
+               })
+               .Build();
+
+await host.RunAsync();

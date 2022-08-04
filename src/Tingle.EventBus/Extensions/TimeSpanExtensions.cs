1519f83fe99d52eed1b2c8e6880deecae9ab28bb
Maxwell Weru <mburumaxwell@gmail.com>
2022-08-04 17:53:18 +0300

    Include Seconds and Milliseconds to ToReadableString(...) extension

--- src/Tingle.EventBus/Extensions/TimeSpanExtensions.cs
+++ src/Tingle.EventBus/Extensions/TimeSpanExtensions.cs
@@ -13,6 +13,8 @@ internal static class TimeSpanExtensions
             span.Days % 7 > 0 ? $"{(span.Days % 7):0} days" : null,
             span.Hours > 0 ? $"{span.Hours:0} hours" : null,
             span.Minutes > 0 ? $"{span.Minutes:0} min" : null,
+            span.Seconds > 0 ? $"{span.Seconds:0} sec" : null,
+            span.Milliseconds > 0 ? $"{span.Milliseconds:0} ms" : null
         }.Where(s => !string.IsNullOrWhiteSpace(s));
 
         return string.Join(", ", parts);

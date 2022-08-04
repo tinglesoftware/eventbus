b045e12614aeca088e2c1b1e50dfcf776f9459d8
Maxwell Weru <mburumaxwell@gmail.com>
2022-08-04 17:52:53 +0300

    Simplify ToReadableString(...) extension

--- src/Tingle.EventBus/Extensions/TimeSpanExtensions.cs
+++ src/Tingle.EventBus/Extensions/TimeSpanExtensions.cs
@@ -7,13 +7,14 @@ internal static class TimeSpanExtensions
     /// <returns></returns>
     public static string ToReadableString(this TimeSpan span)
     {
-        string formatted = string.Format("{0}{1}{2}{3}",
-                                         (span.Days / 7) > 0 ? $"{(span.Days / 7):0} weeks, " : string.Empty,
-                                         span.Days % 7 > 0 ? $"{(span.Days % 7):0} days, " : string.Empty,
-                                         span.Hours > 0 ? $"{span.Hours:0} hours, " : string.Empty,
-                                         span.Minutes > 0 ? $"{span.Minutes:0} min, " : string.Empty);
+        var parts = new List<string?>
+        {
+            (span.Days / 7) > 0 ? $"{(span.Days / 7):0} weeks" : null,
+            span.Days % 7 > 0 ? $"{(span.Days % 7):0} days" : null,
+            span.Hours > 0 ? $"{span.Hours:0} hours" : null,
+            span.Minutes > 0 ? $"{span.Minutes:0} min" : null,
+        }.Where(s => !string.IsNullOrWhiteSpace(s));
 
-        if (formatted.EndsWith(", ")) formatted = formatted[0..^2];
-        return formatted;
+        return string.Join(", ", parts);
     }
 }

a4d671e32ba45038f671d3c3a2dfff9d02d27555
Maxwell Weru <mburumaxwell@gmail.com>
2022-08-08 12:18:39 +0300

    Change NewtonsoftJsonSerializerConfigureOptions and XmlEventSerializerConfigureOptions to use IValidateOptins<T>

--- src/Tingle.EventBus.Serializers.NewtonsoftJson/NewtonsoftJsonSerializerConfigureOptions.cs
+++ src/Tingle.EventBus.Serializers.NewtonsoftJson/NewtonsoftJsonSerializerConfigureOptions.cs
@@ -6,14 +6,16 @@ namespace Microsoft.Extensions.DependencyInjection;
 /// <summary>
 /// A class to finish the configuration of instances of <see cref="NewtonsoftJsonSerializerOptions"/>.
 /// </summary>
-internal class NewtonsoftJsonSerializerConfigureOptions : IPostConfigureOptions<NewtonsoftJsonSerializerOptions>
+internal class NewtonsoftJsonSerializerConfigureOptions : IValidateOptions<NewtonsoftJsonSerializerOptions>
 {
-    public void PostConfigure(string name, NewtonsoftJsonSerializerOptions options)
+    public ValidateOptionsResult Validate(string name, NewtonsoftJsonSerializerOptions options)
     {
         // Ensure the settings are provided
         if (options.SerializerSettings == null)
         {
-            throw new InvalidOperationException($"'{nameof(options.SerializerSettings)}' must be provided");
+            return ValidateOptionsResult.Fail($"'{nameof(options.SerializerSettings)}' must be provided");
         }
+
+        return ValidateOptionsResult.Success;
     }
 }

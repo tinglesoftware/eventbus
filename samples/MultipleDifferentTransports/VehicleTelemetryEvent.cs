93c3643f9075ab2648b391f870e2fb989167265b
Maxwell Weru <mburumaxwell@gmail.com>
2022-10-26 12:23:29 +0300

    Added example for multiple different transports

---
+++ samples/MultipleDifferentTransports/VehicleTelemetryEvent.cs
@@ -0,0 +1,45 @@
+﻿using System.Text.Json.Nodes;
+using System.Text.Json.Serialization;
+using Tingle.EventBus.Transports.Azure.EventHubs.IotHub;
+
+namespace MultipleDifferentTransports;
+
+internal record VehicleTelemetryEvent : IotHubEvent<VehicleTelemetry>
+{
+    public VehicleTelemetryEvent(IotHubEventMessageSource source,
+                                 VehicleTelemetry? telemetry,
+                                 IotHubOperationalEvent<IotHubDeviceTwinChangeEvent>? twinEvent,
+                                 IotHubOperationalEvent<IotHubDeviceLifecycleEvent>? lifecycleEvent,
+                                 IotHubOperationalEvent<IotHubDeviceConnectionStateEvent>? connectionStateEvent)
+        : base(source, telemetry, twinEvent, lifecycleEvent, connectionStateEvent) { }
+}
+
+internal class VehicleTelemetry
+{
+    public DateTimeOffset Timestamp { get; set; }
+
+    public string? Action { get; set; }
+
+    public VehicleDoorKind? VehicleDoorKind { get; set; }
+    public VehicleDoorStatus? VehicleDoorStatus { get; set; }
+
+    [JsonExtensionData]
+    public JsonObject? Extras { get; set; }
+}
+
+public enum VehicleDoorStatus
+{
+    Unknown,
+    Open,
+    Closed,
+}
+
+public enum VehicleDoorKind
+{
+    FrontLeft,
+    FrontRight,
+    RearLeft,
+    ReadRight,
+    Hood,
+    Trunk,
+}

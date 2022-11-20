475619611bbf79c0ed56f86d4873634c5524bc13
Maxwell Weru <mburumaxwell@gmail.com>
2022-11-20 12:26:46 +0300

    Added example for working with Azure Managed Idenitty

---
+++ samples/AzureManagedIdentity/Program.cs
@@ -0,0 +1,60 @@
+﻿using Azure.Identity;
+using AzureManagedIdentity;
+using Tingle.EventBus.Configuration;
+using Tingle.EventBus.Transports.Azure.EventHubs;
+using Tingle.EventBus.Transports.Azure.ServiceBus;
+
+var host = Host.CreateDefaultBuilder(args)
+               .ConfigureServices((hostContext, services) =>
+               {
+                   var configuration = hostContext.Configuration;
+
+                   services.AddEventBus(builder =>
+                   {
+                       builder.Configure(o =>
+                       {
+                           o.DefaultTransportName = AzureServiceBusDefaults.Name;
+
+                           o.ConfigureEvent<VehicleTelemetryEvent>(reg =>
+                           {
+                               reg.ConfigureAsIotHubEvent(configuration["IotHubEventHubName"]!)
+                                  .UseIotHubEventSerializer();
+
+                               reg.TransportName = AzureEventHubsDefaults.Name; // you can also use EventTransportNameAttribute on the event type declaration
+                           });
+                       });
+
+                       builder.AddConsumer<VehicleTelemetryEventsConsumer>();
+
+                       var credential = new DefaultAzureCredential();
+
+                       // Transport specific configuration
+                       builder.AddAzureEventHubsTransport(options =>
+                       {
+                           options.Credentials = new AzureEventHubsTransportCredentials
+                           {
+                               TokenCredential = credential,
+                               FullyQualifiedNamespace = "{your_namespace}.servicebus.windows.net"
+                           };
+                           options.BlobStorageCredentials = new AzureBlobStorageCredentials
+                           {
+                               TokenCredential = credential,
+                               BlobServiceUrl = new Uri("https://{account_name}.blob.core.windows.net"),
+                           };
+                       });
+
+                       // Transport specific configuration
+                       builder.AddAzureServiceBusTransport(options =>
+                       {
+                           options.Credentials = new AzureServiceBusTransportCredentials
+                           {
+                               TokenCredential = credential,
+                               FullyQualifiedNamespace = "{your_namespace}.servicebus.windows.net"
+                           };
+                           options.DefaultEntityKind = EntityKind.Queue; // required if using the basic SKU (does not support topics)
+                       });
+                   });
+               })
+               .Build();
+
+await host.RunAsync();
---
+++ samples/AzureManagedIdentity/VehicleTelemetryEventsConsumer.cs
@@ -0,0 +1,55 @@
+﻿using Tingle.EventBus.Transports.Azure.EventHubs.IotHub;
+
+namespace AzureManagedIdentity;
+
+internal class VehicleTelemetryEventsConsumer : IEventConsumer<VehicleTelemetryEvent>
+{
+    private readonly ILogger logger;
+
+    public VehicleTelemetryEventsConsumer(ILogger<VehicleTelemetryEventsConsumer> logger)
+    {
+        this.logger = logger ?? throw new ArgumentNullException(nameof(logger));
+    }
+
+    public async Task ConsumeAsync(EventContext<VehicleTelemetryEvent> context, CancellationToken cancellationToken)
+    {
+        var evt = context.Event;
+        var source = evt.Source;
+        if (source != IotHubEventMessageSource.Telemetry) return;
+
+        var telemetry = evt.Telemetry!;
+        var action = telemetry.Action;
+        if (action is not "door-status-changed")
+        {
+            logger.LogWarning("Telemetry with action '{TelemetryAction}' is not yet supported", action);
+            return;
+        }
+
+        var status = telemetry.VehicleDoorStatus;
+        if (status is not VehicleDoorStatus.Open and not VehicleDoorStatus.Closed)
+        {
+            logger.LogWarning("Vehicle Door status '{VehicleDoorStatus}' is not yet supported", status);
+            return;
+        }
+
+        var kind = telemetry.VehicleDoorKind;
+        if (kind is null)
+        {
+            logger.LogWarning("Vehicle Door kind '{VehicleDoorKind}' cannot be null", kind);
+            return;
+        }
+
+        var deviceId = context.GetIotHubDeviceId();
+        var timestamp = telemetry.Timestamp;
+        var updateEvt = new VehicleDoorOpenedEvent
+        {
+            VehicleId = deviceId, // not the registration number
+            Kind = kind.Value,
+            Closed = status is VehicleDoorStatus.Closed ? timestamp : null,
+            Opened = status is VehicleDoorStatus.Open ? timestamp : null,
+        };
+
+        // the VehicleDoorOpenedEvent on a broadcast bus would notify all subscribers
+        await context.PublishAsync(updateEvt, cancellationToken: cancellationToken);
+    }
+}

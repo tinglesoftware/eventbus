b08edbe9928b18300c8bf4dec955f1484eb80f6a
Maxwell Weru <mburumaxwell@gmail.com>
2022-12-19 09:39:23 +0300

    Expose TryGetPropertyValue(...) and GetPropertyValue(...)

--- src/Tingle.EventBus.Transports.Azure.EventHubs/EventDataExtensions.cs
+++ src/Tingle.EventBus.Transports.Azure.EventHubs/EventDataExtensions.cs
@@ -8,7 +8,25 @@ namespace Tingle.EventBus.Transports.Azure.EventHubs;
 /// </summary>
 public static class EventDataExtensions
 {
-    internal static bool TryGetPropertyValue(this EventData data, string key, [NotNullWhen(true)] out object? value)
+    /// <summary>
+    /// Gets the property value that is associated with the specified key from
+    /// <see cref="EventData.SystemProperties"/> or <see cref="EventData.Properties"/>.
+    /// </summary>
+    /// <param name="data">The <see cref="EventData"/> instance to use.</param>
+    /// <param name="key">The key to locate.</param>
+    /// <param name="value">
+    /// When this method returns, the value associated with the specified key, if the key is found;
+    /// otherwise, the default value for the type of the value parameter.
+    /// This parameter is passed uninitialized.
+    /// </param>
+    /// <returns>
+    /// <see langword="true"/> true if the <see cref="EventData"/> instance contains a property that
+    /// has the specified key in <see cref="EventData.SystemProperties"/> or <see cref="EventData.Properties"/>;
+    /// otherwise, <see langword="false"/>.
+    /// </returns>
+    /// <exception cref="ArgumentNullException"><paramref name="data"/> is null.</exception>
+    /// <exception cref="ArgumentException"><paramref name="key"/> is null, empty or whitespace.</exception>
+    public static bool TryGetPropertyValue(this EventData data, string key, [NotNullWhen(true)] out object? value)
     {
         if (data is null) throw new ArgumentNullException(nameof(data));
         if (string.IsNullOrWhiteSpace(key))
@@ -20,7 +38,15 @@ public static class EventDataExtensions
             || data.Properties.TryGetValue(key, out value);
     }
 
-    internal static T? GetPropertyValue<T>(this EventData data, string key)
+    /// <summary>
+    /// Gets the property value that is associated with the specified key from
+    /// <see cref="EventData.SystemProperties"/> or <see cref="EventData.Properties"/>.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="data">The <see cref="EventData"/> instance to use.</param>
+    /// <param name="key">The key to locate.</param>
+    /// <returns></returns>
+    public static T? GetPropertyValue<T>(this EventData data, string key)
     {
         return data.TryGetPropertyValue(key, out var value) && value is not null ? (T?)value : default;
     }

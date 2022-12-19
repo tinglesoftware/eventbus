3e02a3ac27fc3d24e96ed0b212da5b4446c58548
Maxwell Weru <mburumaxwell@gmail.com>
2022-12-19 11:18:28 +0300

    Make use of IValidateOptions<T> in TransportOptionsConfigureOptions<T>

--- src/Tingle.EventBus/DependencyInjection/TransportOptionsConfigureOptions.cs
+++ src/Tingle.EventBus/DependencyInjection/TransportOptionsConfigureOptions.cs
@@ -9,8 +9,9 @@ namespace Microsoft.Extensions.DependencyInjection;
 /// Implementation of <see cref="IPostConfigureOptions{TOptions}"/>
 /// for shared settings in <see cref="EventBusTransportOptions"/>.
 /// </summary>
-/// <typeparam name="T"></typeparam>
-internal class TransportOptionsConfigureOptions<T> : IConfigureNamedOptions<T>, IPostConfigureOptions<T> where T : EventBusTransportOptions
+/// <typeparam name="TOptions"></typeparam>
+internal class TransportOptionsConfigureOptions<TOptions> : IConfigureNamedOptions<TOptions>, IPostConfigureOptions<TOptions>, IValidateOptions<TOptions>
+    where TOptions : EventBusTransportOptions
 {
     private readonly IEventBusConfigurationProvider configurationProvider;
 
@@ -25,7 +26,7 @@ internal class TransportOptionsConfigureOptions<T> : IConfigureNamedOptions<T>, 
     }
 
     /// <inheritdoc/>
-    public void Configure(string? name, T options)
+    public void Configure(string? name, TOptions options)
     {
         if (string.IsNullOrEmpty(name)) return;
 
@@ -41,21 +42,26 @@ internal class TransportOptionsConfigureOptions<T> : IConfigureNamedOptions<T>, 
     }
 
     /// <inheritdoc/>
-    public void Configure(T options) => Configure(Options.Options.DefaultName, options);
+    public void Configure(TOptions options) => Configure(Options.Options.DefaultName, options);
 
     /// <inheritdoc/>
-    public void PostConfigure(string? name, T options)
+    public void PostConfigure(string? name, TOptions options)
     {
         // check bounds for empty results delay
         var ticks = options.EmptyResultsDelay.Ticks;
         ticks = Math.Max(ticks, TimeSpan.FromSeconds(30).Ticks); // must be more than 30 seconds
         ticks = Math.Min(ticks, TimeSpan.FromMinutes(10).Ticks); // must be less than 10 minutes
         options.EmptyResultsDelay = TimeSpan.FromTicks(ticks);
+    }
 
+    public ValidateOptionsResult Validate(string? name, TOptions options)
+    {
         // ensure the dead-letter suffix name has been set
         if (string.IsNullOrWhiteSpace(options.DeadLetterSuffix))
         {
-            throw new InvalidOperationException($"The '{nameof(options.DeadLetterSuffix)}' must be provided");
+            return ValidateOptionsResult.Fail($"'{nameof(options.DeadLetterSuffix)}' must be provided.");
         }
+
+        return ValidateOptionsResult.Success;
     }
 }

ea5fd7c7056a11171321f91ed36894ea16cfbbf3
Maxwell Weru <mburumaxwell@gmail.com>
2023-05-26 13:17:44 +0300

    Add setter for serializable types to allow JSON source generators

--- src/Tingle.EventBus/Serialization/HostInfo.cs
+++ src/Tingle.EventBus/Serialization/HostInfo.cs
@@ -9,35 +9,35 @@ public record HostInfo
     /// The machine name (or role instance name) of the machine.
     /// </summary>
     /// <example>WIN-HQ1243</example>
-    public virtual string? MachineName { get; init; }
+    public virtual string? MachineName { get; set; }
 
     /// <summary>
     /// The name of the application.
     /// </summary>
     /// <example>Tingle.EventBus.Examples.SimplePublisher</example>
-    public virtual string? ApplicationName { get; init; }
+    public virtual string? ApplicationName { get; set; }
 
     /// <summary>
     /// The version of the application.
     /// </summary>
     /// <example>1.0.0.0</example>
-    public virtual string? ApplicationVersion { get; init; }
+    public virtual string? ApplicationVersion { get; set; }
 
     /// <summary>
     /// The name of the environment the application is running in.
     /// </summary>
     /// <example>Production</example>
-    public virtual string? EnvironmentName { get; init; }
+    public virtual string? EnvironmentName { get; set; }
 
     /// <summary>
     /// The version of the library.
     /// </summary>
     /// <example>1.0.0.0</example>
-    public virtual string? LibraryVersion { get; init; }
+    public virtual string? LibraryVersion { get; set; }
 
     /// <summary>
     /// The operating system hosting the application.
     /// </summary>
     /// <example>Microsoft Windows NT 10.0.19042.0</example>
-    public virtual string? OperatingSystem { get; init; }
+    public virtual string? OperatingSystem { get; set; }
 }
